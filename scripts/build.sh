2a3f90c29738acee05f6cdb78a23ffd439fdaa7c
eepMoody <moody.designer@gmail.com>
2019-01-21 17:46:54 -0500

    how about this?

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 
-heroku config:unset PYTHONHOME
 pipenv run pip install -r ./requirements.txt
 pipenv run python manage.py migrate
 pipenv run python manage.py populatedb --flush ./data/WOTC_5e_SRD_v5.1/
