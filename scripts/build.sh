43b059ca37694d05702043fc770450276aa9641a
eepMoody <moody.designer@gmail.com>
2019-01-21 17:30:13 -0500

    post build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
-pipenv run pip install requirements.txt
+pipenv run pip install -r ./requirements.txt
 pipenv run python manage.py migrate
-pipenv run python manage.py populatedb --flush /data/WOTC_5e_SRD_v5.1/
+pipenv run python manage.py populatedb --flush ./data/WOTC_5e_SRD_v5.1/
