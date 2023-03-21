d47f11f5705fb3dd0542859f218b97c144885ea1
BuildTools <unconfigured@null.spigotmc.org>
2023-03-20 21:38:16 -0500

    Filters are now filtersets in current DRF

--- server/settings.py
+++ server/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'api.utils.StandardResultsSetPagination',
 
     # Filtering
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.OrderingFilter','rest_framework.filters.SearchFilter',),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend','rest_framework.filters.OrderingFilter','rest_framework.filters.SearchFilter'],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': [],
