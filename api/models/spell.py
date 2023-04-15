a9738cb1d303f8bf5da91e9f42ffc0e38d8df498
BuildTools <unconfigured@null.spigotmc.org>
2023-04-15 15:48:41 -0500

    Formatting with pep8

--- api/models/spell.py
+++ api/models/spell.py
@@ -55,30 +55,33 @@ class Spell(GameContent):
         help_text='What happens if you cast this at a higher level.')
     page = models.TextField(
         help_text='Page number reference for the document.')
-    
+
     range = models.TextField(help_text='Text description of the range.')
-    target_range_sort = models.IntegerField(help_text='Sortable distance ranking to the target.')
+    target_range_sort = models.IntegerField(
+        help_text='Sortable distance ranking to the target.')
 
     components = models.TextField(
         help_text='Single-character list of V, S, M for Verbal, Somatic, or Material based on the spell requirements.')
     material = models.TextField(
         help_text='Description of the material required.')
-    
+
     can_be_cast_as_ritual = models.BooleanField(
         help_text='Whether or not the spell can be cast as a ritual.')
     # Maintaining compatibility with the old "ritual" string field.
+
     def v1_ritual(self):
         if self.can_be_cast_as_ritual:
             return "yes"
         else:
             return "no"
-    
+
     duration = models.TextField(
         help_text='Description of the duration such as "instantaneous" or "Up to 1 minute"')
-    
+
     requires_concentration = models.BooleanField(
         help_text='Whether the spell requires concentration')
     # Maintaining compatibility with the old "concentration" string field
+
     def v1_concentration(self):
         if self.requires_concentration:
             return "yes"
@@ -87,11 +90,11 @@ class Spell(GameContent):
 
     casting_time = models.TextField(
         help_text='Amount of time it takes to cast the spell, such as "1 bonus action" or "4 hours".')
-    
-    
+
     spell_level = models.IntegerField(
         help_text='Integer representing the level of the spell. Cantrip is 0.')
     # Maintaining compatibility with the old string field.
+
     def v1_level(self):
         return self.spell_level_name_lookup[self.spell_level]
 
@@ -116,9 +119,10 @@ class Spell(GameContent):
             self.page = json["page"]
         # Logic to set an integer based on v1 file import spec (a string).
         if "range" in json:
-            # If the spell range includes an effect description like "(10 ft cube)", remove.
+            # If the spell range includes an effect description like "(10 ft
+            # cube)", remove.
             range_trimmed = str(json['range'].split(" (")[0]).lower()
-            
+
             # Set target range sort based on the following rules:
             # Self = 0
             # Touch = 1
@@ -126,13 +130,13 @@ class Spell(GameContent):
             # "Sight" is recorded as 9999
             # Unlimited (same-plane) is recorded as 99990
             # Unlimited (multi-plane) is recorded as 99999
-            
+
             if 'self' in range_trimmed:
                 # Various spells in deep magic include effect shape.
                 self.target_range_sort = 0
             if range_trimmed == 'touch':
                 self.target_range_sort = 1
-            
+
             if range_trimmed == 'sight':
                 self.target_range_sort = 9999
 
@@ -142,8 +146,9 @@ class Spell(GameContent):
             if range_trimmed == 'special':
                 # For now, assume it's single plane
                 self.target_range_sort = 99990
-                            
-            if range_trimmed.endswith('miles') or range_trimmed.endswith('mile'):
+
+            if range_trimmed.endswith(
+                    'miles') or range_trimmed.endswith('mile'):
                 # Assume it's in the format "xx Miles"
                 miles = int(json['range'].split(" ")[0])
                 self.target_range_sort = miles * 5280
@@ -160,12 +165,12 @@ class Spell(GameContent):
             self.material = json["material"]
 
         # Logic to set boolean based on v1 file import spec (a string).
-        self.can_be_cast_as_ritual = False        
+        self.can_be_cast_as_ritual = False
         if "ritual" in json:
             # Default is false
             if str(json["ritual"]).lower() == 'yes':
                 self.can_be_cast_as_ritual = True
-            
+
             if str(json["ritual"]).lower() == 'no':
                 pass  # Already set as False through default.
 
@@ -178,19 +183,19 @@ class Spell(GameContent):
             # Default is true
             if str(json['concentration']).lower() == 'no':
                 self.requires_concentration = False
-            
+
             if str(json["concentration"]).lower() == 'yes':
-                pass #Already set to True through default.
-            
+                pass  # Already set to True through default.
+
         if "casting_time" in json:
             self.casting_time = json["casting_time"]
         if "level" in json:
             self.level = json["level"]
-        
+
         # Model field is renamed to spell_level
         if "level_int" in json:
             self.spell_level = json["level_int"]
-        
+
         if "school" in json:
             self.school = json["school"]
         if "class" in json:
