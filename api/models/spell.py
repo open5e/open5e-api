cbc19f5db6fc525d656ef29986a3321c0c114eda
BuildTools <unconfigured@null.spigotmc.org>
2023-04-15 15:47:34 -0500

    Moving the importer into the model.

--- api/models/spell.py
+++ api/models/spell.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 from .models import GameContent
 
@@ -103,6 +104,101 @@ class Spell(GameContent):
     circles = models.TextField(
         help_text='Druid Archetypes that can learn this spell.')
     route = models.TextField(default="spells/")
+
+    def import_from_json_v1(self, json):
+        self.name = json["name"]
+        self.slug = slugify(json["name"])
+        if "desc" in json:
+            self.desc = json["desc"]
+        if "higher_level" in json:
+            self.higher_level = json["higher_level"]
+        if "page" in json:
+            self.page = json["page"]
+        # Logic to set an integer based on v1 file import spec (a string).
+        if "range" in json:
+            # If the spell range includes an effect description like "(10 ft cube)", remove.
+            range_trimmed = str(json['range'].split(" (")[0]).lower()
+            
+            # Set target range sort based on the following rules:
+            # Self = 0
+            # Touch = 1
+            # Any other distance is recorded in feet.
+            # "Sight" is recorded as 9999
+            # Unlimited (same-plane) is recorded as 99990
+            # Unlimited (multi-plane) is recorded as 99999
+            
+            if 'self' in range_trimmed:
+                # Various spells in deep magic include effect shape.
+                self.target_range_sort = 0
+            if range_trimmed == 'touch':
+                self.target_range_sort = 1
+            
+            if range_trimmed == 'sight':
+                self.target_range_sort = 9999
+
+            if range_trimmed == 'unlimited':
+                self.target_range_sort = 99999
+
+            if range_trimmed == 'special':
+                # For now, assume it's single plane
+                self.target_range_sort = 99990
+                            
+            if range_trimmed.endswith('miles') or range_trimmed.endswith('mile'):
+                # Assume it's in the format "xx Miles"
+                miles = int(json['range'].split(" ")[0])
+                self.target_range_sort = miles * 5280
+
+            if range_trimmed.endswith('feet') or range_trimmed.endswith('ft.'):
+                # Assume it's in the format "xx Miles"
+                feet = int(range_trimmed.split(" ")[0])
+                self.target_range_sort = feet
+
+            self.range = json["range"]
+        if "components" in json:
+            self.components = json["components"]
+        if "material" in json:
+            self.material = json["material"]
+
+        # Logic to set boolean based on v1 file import spec (a string).
+        self.can_be_cast_as_ritual = False        
+        if "ritual" in json:
+            # Default is false
+            if str(json["ritual"]).lower() == 'yes':
+                self.can_be_cast_as_ritual = True
+            
+            if str(json["ritual"]).lower() == 'no':
+                pass  # Already set as False through default.
+
+        if "duration" in json:
+            self.duration = json["duration"]
+
+        # Logic to set a boolean based on v1 file import spec (a string).
+        self.requires_concentration = True
+        if "concentration" in json:
+            # Default is true
+            if str(json['concentration']).lower() == 'no':
+                self.requires_concentration = False
+            
+            if str(json["concentration"]).lower() == 'yes':
+                pass #Already set to True through default.
+            
+        if "casting_time" in json:
+            self.casting_time = json["casting_time"]
+        if "level" in json:
+            self.level = json["level"]
+        
+        # Model field is renamed to spell_level
+        if "level_int" in json:
+            self.spell_level = json["level_int"]
+        
+        if "school" in json:
+            self.school = json["school"]
+        if "class" in json:
+            self.dnd_class = json["class"]
+        if "archetype" in json:
+            self.archetype = json["archetype"]
+        if "circles" in json:
+            self.circles = json["circles"]
 
     @staticmethod
     def plural_str() -> str:
