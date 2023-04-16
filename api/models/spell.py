25b876f979e49d8503ca7aa4f0d14625d6724386
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 08:59:05 -0500

    Reorganizing fields, adding validators.

--- api/models/spell.py
+++ api/models/spell.py
@@ -6,12 +6,93 @@ Also includes importing and some presentation logic.
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
 from .models import GameContent
 
 
 class Spell(GameContent):
     """The model for a spell."""
+
+    # Stored metadata about the spell
+    page = models.TextField(
+        help_text='Page number reference for the document.')
+
+    spell_level = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(9)],
+        help_text='Integer representing the level of the spell. Cantrip is 0.')
+
+    dnd_class = models.TextField(
+        help_text='List of classes (comma separated) that can learn this spell.')
+
+    school = models.TextField(
+        help_text='Representation of the school of magic, such as "illusion" or "evocation".')
+
+    # Computed metadata about the spell
+    def v1_level(self):
+        """Presents the spell level in a friendly format."""
+        return self.spell_level_name_lookup[self.spell_level]
+
+    # Stored data about casting the spell
+    casting_time = models.TextField(
+        help_text='Amount of time it takes to cast the spell, such as "1 bonus action" or "4 hours".')
+
+    range = models.TextField(help_text='Text description of the target range.')
+
+    target_range_sort = models.IntegerField(
+        validators=[MinValueValidator(0)],
+        help_text='Sortable distance ranking to the target.')
+
+    requires_verbal_components = models.BooleanField(
+        help_text='Casting this spell requires verbal components.')
+    requires_somatic_components = models.BooleanField(
+        help_text='Casting this spell requires somatic components.')
+    requires_material_components = models.BooleanField(
+        help_text='Casting this spell requires material components.')
+
+    material = models.TextField(
+        help_text='Description of the material required.')
+
+    higher_level = models.TextField(
+        help_text='What happens if you cast this at a higher level.')
+
+    can_be_cast_as_ritual = models.BooleanField(
+        help_text='Whether or not the spell can be cast as a ritual.')
+
+    # Computed data about casting the spell
+    def v1_components(self):
+        """Presents the components in a standard list like "V, S, M."""
+        components_list = []
+        if self.requires_verbal_components:
+            components_list.append("V")
+        if self.requires_somatic_components:
+            components_list.append("S")
+        if self.requires_material_components:
+            components_list.append("M")
+
+        return ', '.join(components_list)
+
+    def v1_ritual(self):
+        """Field calculated and presented to maintain compatibility with original API."""
+        if self.can_be_cast_as_ritual:
+            return "yes"
+        else:
+            return "no"
+
+    # Stored data about the spell's effect
+    duration = models.TextField(
+        help_text='Description of the duration such as "instantaneous" or "Up to 1 minute"')
+
+    requires_concentration = models.BooleanField(
+        help_text='Whether the spell requires concentration')
+
+    def v1_concentration(self):
+        """Field calculated and presented to maintain compatibility with original API."""
+        if self.requires_concentration:
+            return "yes"
+        else:
+            return "no"
 
     spell_level_name_lookup = [
         'Cantrip',
@@ -58,77 +139,6 @@ class Spell(GameContent):
         'thunder'
     ]
 
-    higher_level = models.TextField(
-        help_text='What happens if you cast this at a higher level.')
-    page = models.TextField(
-        help_text='Page number reference for the document.')
-
-    range = models.TextField(help_text='Text description of the range.')
-    target_range_sort = models.IntegerField(
-        help_text='Sortable distance ranking to the target.')
-
-    requires_verbal_components = models.BooleanField(
-        help_text='Casting this spell requires verbal components.')
-    requires_somatic_components = models.BooleanField(
-        help_text='Casting this spell requires somatic components.')
-    requires_material_components = models.BooleanField(
-        help_text='Casting this spell requires material components.')
-
-    def v1_components(self):
-        """Presents the components in a standard list like "V, S, M."""
-        components_list = []
-        if self.requires_verbal_components:
-            components_list.append("V")
-        if self.requires_somatic_components:
-            components_list.append("S")
-        if self.requires_material_components:
-            components_list.append("M")
-
-        return ', '.join(components_list)
-
-    material = models.TextField(
-        help_text='Description of the material required.')
-
-    can_be_cast_as_ritual = models.BooleanField(
-        help_text='Whether or not the spell can be cast as a ritual.')
-    # Maintaining compatibility with the old "ritual" string field.
-
-    def v1_ritual(self):
-        """Field calculated and presented to maintain compatibility with original API."""
-        if self.can_be_cast_as_ritual:
-            return "yes"
-        else:
-            return "no"
-
-    duration = models.TextField(
-        help_text='Description of the duration such as "instantaneous" or "Up to 1 minute"')
-
-    requires_concentration = models.BooleanField(
-        help_text='Whether the spell requires concentration')
-    # Maintaining compatibility with the old "concentration" string field
-
-    def v1_concentration(self):
-        """Field calculated and presented to maintain compatibility with original API."""
-        if self.requires_concentration:
-            return "yes"
-        else:
-            return "no"
-
-    casting_time = models.TextField(
-        help_text='Amount of time it takes to cast the spell, such as "1 bonus action" or "4 hours".')
-
-    spell_level = models.IntegerField(
-        help_text='Integer representing the level of the spell. Cantrip is 0.')
-    # Maintaining compatibility with the old string field.
-
-    def v1_level(self):
-        """Presents the spell level in a friendly format."""
-        return self.spell_level_name_lookup[self.spell_level]
-
-    school = models.TextField(
-        help_text='Representation of the school of magic, such as "illusion" or "evocation".')
-    dnd_class = models.TextField(
-        help_text='List of classes (comma separated) that can learn this spell.')
     archetype = models.TextField(
         help_text='Archetype that can learn this spell. If empty, assume all archetypes.')
     circles = models.TextField(
