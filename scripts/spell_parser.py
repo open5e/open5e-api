5b118838785bf285bcc94058d43a5ace01c01943
BuildTools <unconfigured@null.spigotmc.org>
2023-04-20 12:14:02 -0500

    pushes data to console.

--- scripts/spell_parser.py
+++ scripts/spell_parser.py
@@ -2,6 +2,7 @@ import sys
 import os
 import argparse
 import json
+import re
 
 from pathlib import Path
 
@@ -46,6 +47,7 @@ def main():
             print("Opening and parsing {}".format(file.name))
             file_json = json.load(file.open())
             keyword_list = ['sphere', 'line', 'cone', 'cube', 'cylinder']
+            context_word_list = ['feet', 'foot']
             attribute_name = 'shape'
             results = []
             for item in file_json:
@@ -55,18 +57,21 @@ def main():
                 for keyword in keyword_list:
                     #print("Keyword: {}".format(keyword))
                     analysis = find_keyword_in_string(item['desc'], keyword)
+
                     result['keywordfound']=analysis[0]
                     if result['keywordfound']==True:
-                        choice = 0
-                        while choice not in ['1','2']:
-                            print(slugify(item['name']) + "     " + keyword + "     " + analysis[1])
+                        context_exists = find_keyword_context_in_string(item['desc'], keyword, 3,context_word_list)
+                        print(slugify(item['name']) + "     " + keyword + "     " + analysis[1])
+                        if context_exists:
+                            choice='1'
+                        else:
                             choice = input('1: Tag it\n2: Skip\n'.format(attribute_name, keyword, slugify(item['name'])))
-                            if choice == '1':
-                                print(slugify(item['name']) + " tagged with " + keyword)
-                                result['modified']=True
-                                result['shape']=keyword
-                            if choice == '2':
-                                print("skipping")
+                        if choice == '1':
+                            print(slugify(item['name']) + " tagged with " + keyword)
+                            result['modified']=True
+                            result['shape']=keyword
+                        if choice == '2':
+                            print("skipping")
                 results.append(result)
         print(results)
                 
@@ -80,6 +85,14 @@ def find_keyword_in_string(string, keyword):
         return (True, context)
     else:
         return (False, None)
+
+def find_keyword_context_in_string(string, keyword, distance, cwl):
+    context = string[string.index(keyword)-40:string.index(keyword)+40]
+    for context_word in cwl:
+        if context_word in context:
+            return True
+
+    return False
 
 
 if __name__ == '__main__':
