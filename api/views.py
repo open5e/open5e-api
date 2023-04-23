415932c430d56120c9efec58f47b810ed37c9fe9
BuildTools <unconfigured@null.spigotmc.org>
2023-04-22 21:43:39 -0500

    Removing an inaccurate comment, Adding bool filter

--- api/views.py
+++ api/views.py
@@ -94,8 +94,6 @@ class DocumentViewSet(viewsets.ReadOnlyModelViewSet):
         )
 
 class SpellFilter(django_filters.FilterSet):
-    # TODO the filter for level_int was lost in the modelfield rename, replace this.
-    
     level_int = django_filters.NumberFilter(field_name='spell_level')
     concentration = django_filters.CharFilter(field_name='concentration')
     components = django_filters.CharFilter(field_name='components')
@@ -110,9 +108,12 @@ class SpellFilter(django_filters.FilterSet):
             'school': ['iexact', 'exact', 'in', ],
             'duration': ['iexact', 'exact', 'in', ],
             'requires_concentration': ['exact'],
+            'requires_verbal_components': ['exact'],
+            'requires_somatic_components': ['exact'],
+            'requires_material_components': ['exact'],
             'casting_time': ['iexact', 'exact', 'in', ],
             'dnd_class': ['iexact', 'exact', 'in', 'icontains'],
-            'document__slug': ['iexact', 'exact', 'in', ],
+            'document__slug': ['iexact', 'exact', 'in', ]
         }
 
 
