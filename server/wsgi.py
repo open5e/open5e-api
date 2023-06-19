37e6461a8aefd91b28145616eb927bba4801f221
Ean Moody <moody.designer@gmail.com>
2023-06-19 18:35:09 -0400

    add newrelic telemetry

--- server/wsgi.py
+++ server/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import newrelic.agent
+newrelic_config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../newrelic.ini')
+newrelic.agent.initialize(newrelic_config_file)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
