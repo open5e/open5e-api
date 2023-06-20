933ded16b8392b3ab045666401c9ff8ba6d09d46
Ean Moody <moody.designer@gmail.com>
2023-06-19 20:22:23 -0400

    push up correct config for logging

--- server/wsgi.py
+++ server/wsgi.py
@@ -8,12 +8,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import newrelic.agent
+
+newrelic.agent.initialize('newrelic.ini', os.getenv('NEW_RELIC_ENVIRONMENT', 'development')
 
 from django.core.wsgi import get_wsgi_application
-
-import newrelic.agent
-newrelic_config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../newrelic.ini')
-newrelic.agent.initialize(newrelic_config_file)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
