ced246d513e5ee228ba72b2ac2524b7a4966d059
Ean Moody <moody.designer@gmail.com>
2023-06-21 01:12:14 -0400

    add middleware for referrer check

--- server/settings.py
+++ server/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "server.middleware.NewRelicMiddleware",
 ]
 
 HAYSTACK_CONNECTIONS = {
