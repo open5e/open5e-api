89c805907f1368ebee581a697f3a30bb842cc917
BuildTools <unconfigured@null.spigotmc.org>
2023-07-02 14:38:13 -0500

    Adding filters.

--- api_v2/views.py
+++ api_v2/views.py
@@ -24,7 +24,7 @@ class ItemFilterSet(FilterSet):
             'category': ['in', 'iexact', 'exact'],
             'document__key': ['in','iexact','exact']
         }
-# Create your views here.
+
 
 class ItemViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -37,6 +37,17 @@ class ItemViewSet(viewsets.ReadOnlyModelViewSet):
     filterset_class = ItemFilterSet
 
 
+class ItemSetFilterSet(FilterSet):
+
+    class Meta:
+        model = models.ItemSet
+        fields = {
+            'key': ['in', 'iexact', 'exact' ],
+            'name': ['iexact', 'exact'],
+            'document__key': ['in','iexact','exact']
+        }
+
+
 class ItemSetViewSet(viewsets.ReadOnlyModelViewSet):
     """"
     list: API Endpoint for returning a set of itemsets.
@@ -45,6 +56,7 @@ class ItemSetViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = models.ItemSet.objects.all().order_by('pk')
     serializer_class = serializers.ItemSetSerializer
+    filterset_class = ItemSetFilterSet
 
 
 class RulesetViewSet(viewsets.ReadOnlyModelViewSet):
@@ -88,7 +100,31 @@ class LicenseViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class WeaponFilterSet(FilterSet):
-    pass
+
+    class Meta:
+        model = models.Weapon
+        fields = {
+            'key': ['in', 'iexact', 'exact' ],
+            'name': ['iexact', 'exact'],
+            'document__key': ['in','iexact','exact'],
+            'damage_type': ['in','iexact','exact'],
+            'damage_dice': ['in','iexact','exact'],
+            'versatile_dice': ['in','iexact','exact'],
+            'range_reach': ['exact','lt','lte','gt','gte'],
+            'range_normal': ['exact','lt','lte','gt','gte'],
+            'range_long': ['exact','lt','lte','gt','gte'],
+            'is_finesse': ['exact'],
+            'is_thrown': ['exact'],
+            'is_two_handed': ['exact'],
+            'requires_ammunition': ['exact'],
+            'requires_loading': ['exact'],
+            'is_heavy': ['exact'],
+            'is_light': ['exact'],
+            'is_lance': ['exact'],
+            'is_net': ['exact'],
+            'is_simple': ['exact'],
+            'is_improvised': ['exact']
+            }
 
 
 class WeaponViewSet(viewsets.ReadOnlyModelViewSet):
@@ -98,7 +134,25 @@ class WeaponViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = models.Weapon.objects.all().order_by('pk')
     serializer_class = serializers.WeaponSerializerFull
-    filterset_fields = '__all__'
+    filterset_class = WeaponFilterSet
+
+
+class ArmorFilterSet(FilterSet):
+
+    class Meta:
+        model = models.Armor
+        fields = {
+            'key': ['in', 'iexact', 'exact' ],
+            'name': ['iexact', 'exact'],
+            'document__key': ['in','iexact','exact'],
+            'grants_stealth_disadvantage': ['exact'],
+            'strength_score_required': ['exact','lt','lte','gt','gte'],
+            'ac_base': ['exact','lt','lte','gt','gte'],
+            'ac_add_dexmod': ['exact'],
+            'ac_cap_dexmod': ['exact'],
+
+        }
+
 
 
 class ArmorViewSet(viewsets.ReadOnlyModelViewSet):
@@ -108,4 +162,4 @@ class ArmorViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = models.Armor.objects.all().order_by('pk')
     serializer_class = serializers.ArmorSerializerFull
-    filterset_fields = '__all__'
+    filterset_class = ArmorFilterSet
