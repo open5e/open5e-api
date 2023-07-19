8f18e94c279bed98e508ac85a25624c7eff6af8e
BuildTools <unconfigured@null.spigotmc.org>
2023-07-19 07:36:16 -0500

    parsing fun.

--- scripts/datafile_parser.py
+++ scripts/datafile_parser.py
@@ -19,6 +19,16 @@ def main():
         parser.add_argument('-r', '--recursive', action='store_true',
             help='Find filename recursively below datadir.')
 
+        parser.add_argument('--modifiedsuffix', default='_modified',
+            help='Suffix for the output filename of changed items.')
+
+        parser.add_argument('--unmodifiedsuffix', default='_unchanged',
+            help='Suffix for the output filename of unchanged items.')
+
+
+        parser.add_argument('-t', '--test', action='store_true', default=False,
+            help='Do not write output files.')
+
         args = parser.parse_args()
 
         if len(sys.argv) < 2:
@@ -47,16 +57,15 @@ def main():
             print("Opening and parsing {}".format(file.name))
             file_json = json.load(file.open())
             print("File Loaded")
-            #item_model={"mode":"api_v2.item","fields":{}}
 
             modified_items = []
             unprocessed_items = []
             for item in file_json:
-                #print(item['type'])
-
                 any_armor = ['studded-leather','splint','scale-mail','ring-mail','plate','padded','leather','hide','half-plate','chain-shirt','chain-mail','breastplate']
                 light = ['studded-leather','padded','leather']
-                armor_med_heavy = ['splint','scale-mail','ring-mail','plate','hide','half-plate','chain-shirt','chain-mail','breastplate']
+                any_med_heavy = ['splint','scale-mail','ring-mail','plate','hide','half-plate','chain-shirt','chain-mail','breastplate']
+                any_heavy = ['splint','ring-mail','plate','chain-mail']
+
                 any_sword_slashing = ['shortsword','longsword','greatsword', 'scimitar']
                 any_axe = ['handaxe','battleaxe','greataxe']
                 any_weapon = [
@@ -70,21 +79,11 @@ def main():
                     'quarterstaff',
                     'sickle',
                     'spear',
-                    'crossbow-light',
-                    'dart',
-                    'shortbow',
-                    'sling',
                     'battleaxe',
                     'flail',
-                    'glaive',
-                    'greataxe',
-                    'greatsword',
-                    'halberd',
                     'lance',
                     'longsword',
-                    'maul',
                     'morningstar',
-                    'pike'
                     'rapier',
                     'scimitar',
                     'shortsword',
@@ -93,64 +92,80 @@ def main():
                     'warhammer',
                     'whip',
                     'blowgun',
-                    'crossbow-hand',
-                    'crossbow-heavy',
-                    'longbow',
                     'net']
-                #any_sword_slashing = ['shortsword','longsword','greatsword']
 
                 item_model={"model":"api_v2.item"}
+                item_model['pk'] = slugify(item['name'])
                 item_model['fields'] = dict({})
-                #item_model['pk']=slugify(item["name"])
-                #item_model['fields']['name']=item["name"]
+                item_model['fields']['name'] = item['name']
                 item_model['fields']['desc']=item["desc"]
-                item_model['fields']['category']="armor"
                 item_model['fields']['size']=1
-                item_model['fields']['weight']=0.0
+                item_model['fields']['weight']=str(0.0)
                 item_model['fields']['armor_class']=0
                 item_model['fields']['hit_points']=0
-                item_model['fields']['document']="srd"
-                item_model['fields']['cost']=None
+                item_model['fields']['document']="vault-of-magic"
+                item_model['fields']['cost']=0
                 item_model['fields']['weapon']=None
                 item_model['fields']['armor']=None
                 item_model['fields']['requires_attunement']=False
                 if "requires-attunement" in item:
                     if item["requires-attunement"]=="requires attunement":
                         item_model['fields']['requires_attunement']=True
-                if item["rarity"] not in ['common','uncommon','rare','very rare','legendary']:
-                    #print(item['name'], item['rarity'])
-                    unprocessed_items.append(item)
-                    continue
-                if item['type'] != "Armor (studded leather)":
+                #if item["rarity"] not in ['common','uncommon','rare','very rare','legendary']:
+                #    print(item['name'], item['rarity'])
+                #    unprocessed_items.append(item)
+                #    continue
+
+                if item['rarity'] == 'common':
+                    item_model['fields']['rarity'] = 1
+                if item['rarity'] == 'uncommon':
+                    item_model['fields']['rarity'] = 2
+                if item['rarity'] == 'rare':
+                    item_model['fields']['rarity'] = 3
+                if item['rarity'] == 'very rare':
+                    item_model['fields']['rarity'] = 4
+                if item['rarity'] == 'legendary':
+                    item_model['fields']['rarity'] = 5
+
+                if item['type'] != "Potion":
                     unprocessed_items.append(item)
                     continue
                 
-                for armor in ['studded-leather']:
+                if 'Unstable Bombard' not in item['name']:
+                    unprocessed_items.append(item)
+                    continue
+
+                item_model['fields']['category']="potion"
+                item_model['fields']['rarity'] = 3
+
+                for f in ["mindshatter","murderous","sloughide"]:
                    # for x,rar in enumerate(['uncommon','rare','very rare']):
-                    item_model['fields']['armor'] = armor
-                    item_model['fields']['rarity'] = 3
-                    item_model['fields']['name']= "{}".format(item['name'])
+                    item_model['fields']['name']= "{} Bombard".format(f.title())
+                    #item_model['fields']['name'] = item['name']
+                    #item_model['fields']['armor'] = 'leather'
                     item_model['pk'] = slugify(item_model['fields']["name"])
                     print_item = json.loads(json.dumps(item_model))
                     modified_items.append(print_item)
-                    #print("Just added:{}".format(item_model['fields']['rarity']))
-                    print("Counter:{}".format(len(modified_items)))
 
-                item_model = {}
-
-
+#                modified_items.append(item_model)
 
             print("Unprocessed count:{}".format(len(unprocessed_items)))
             print("Processed count:  {}".format(len(modified_items)))
             
            
-            if True:
-                sister_file = str(file.parent)+"/"+file.stem + "_modified" + file.suffix
-                with open(sister_file, 'w', encoding='utf-8') as s:
+            if not args.test:
+                
+                modified_file = str(file.parent)+"/"+file.stem + args.modifiedsuffix + file.suffix
+                print('Writing modified objects to {}.'.format(modified_file))
+                if(os.path.isfile(modified_file)):
+                    print("File already exists!")
+                    exit(0)
+                with open(modified_file, 'w', encoding='utf-8') as s:
                     s.write(json.dumps(modified_items, ensure_ascii=False, indent=4))
                 
-                unprocced = str(file.parent)+"/"+file.stem + "_unprocessed" + file.suffix
-                with open(unprocced, 'w', encoding='utf-8') as s:
+                unmodified_file = str(file.parent)+"/"+file.stem + args.unmodifiedsuffix + file.suffix
+                print('Writing unmodified objects to {}.'.format(unmodified_file))
+                with open(unmodified_file, 'w', encoding='utf-8') as s:
                     s.write(json.dumps(unprocessed_items, ensure_ascii=False, indent=4))
             
 
