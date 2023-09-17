d6fdf3d3a01bb021794548d54567cfb28491dc89
BuildTools <unconfigured@null.spigotmc.org>
2023-09-17 05:50:12 -0500

    Stubbing out languages.

---
+++ api_v2/models/languages.py
@@ -0,0 +1,37 @@
+"""The model for a language."""
+
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+
+from api.models import GameContent
+from .weapon import Weapon
+from .armor import Armor
+from .abstracts import Object, HasName, HasDescription
+from .document import FromDocument
+
+
+class Language(HasName, HasDescription, FromDocument):
+    """This is the model for a language, which is a form of communication."""
+
+    script_language = models.ForeignKey('self',
+                                        null=True,
+                                        on_delete=models.CASCADE)
+
+    # TODO typical_speakers will be a FK out to a Creature Types table.
+
+    is_exotic = models.BooleanField(
+        help_text='Whether or not the language is exotic.',
+        default=False)
+
+    is_secret = models.BooleanField(
+        help_text='Whether or not the language is secret.',
+        default=False)
+
+
+class LanguageSet(HasName, HasDescription, FromDocument):
+    """A set of languages to be referenced."""
+
+    languages = models.ManyToManyField(Item,
+                                       related_name="languagesets",
+                                       help_text="The set of languages.")
