3c370dbbdb45fdadd23056e94cd81eebd66c71dd
BuildTools <unconfigured@null.spigotmc.org>
2023-09-17 06:34:02 -0500

    Adjusting exports.

--- api_v2/management/commands/export.py
+++ api_v2/management/commands/export.py
@@ -76,10 +76,9 @@ class Command(BaseCommand):
                 for model in app_models:
                     SKIPPED_MODEL_NAMES = ['Document']
                     if model._meta.app_label == 'api_v2' and model.__name__ not in SKIPPED_MODEL_NAMES:
-                        if model.__name__ in ['Trait']:
-                            modelq = model.objects.filter(race__document=doc).order_by('pk')
-                        else:
-                            modelq = model.objects.filter(document=doc).order_by('pk')
+
+                        modelq = get_model_queryset_by_document(model, doc)
+
                         model_path = get_filepath_by_model(
                             model.__name__,
                             model._meta.app_label,
@@ -129,3 +128,17 @@ def write_queryset_data(filepath, queryset):
         output_filepath = filepath
         with open(output_filepath, 'w', encoding='utf-8') as f:
             serializers.serialize("json", queryset, indent=2, stream=f)
+
+def get_model_queryset_by_document(model, doc):
+    print("Getting the queryset for: {}".format(model.__name__))
+
+    if model.__name__ in ['Trait']:
+        return model.objects.filter(race__document=doc).order_by('pk')
+
+    if model.__name__ in ['BackgroundBenefit', 'Characteristics', 'BackgroundFeature']:
+        return model.objects.filter(background__document=doc).order_by('pk')
+
+    if model.__name__ in ['FeatBenefit']:
+        return model.objects.filter(feat__document=doc).order_by('pk')
+
+    return model.objects.filter(document=doc).order_by('pk')
