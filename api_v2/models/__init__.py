063f82d2e42361f1e839d748103ae5959db749ad
Michael Groufsky <wardsky@users.noreply.github.com>
2023-10-03 03:16:23 +1300

    Implement initial Creature model, including Abilities (#317)
    * Incomplete Creature model
    
    With sample monsters for testing.
    
    * Implement abilities model
    
    * Create api_v2 versions of every monster from the SRD
    
    Currently includes only attributes and abilities.
    
    * Add creature actions model and data for v2
    
    * Add monster attack action stats
    
    * Add documentation for creature action fields
    
    * Add category, type, subtype, and alignment fields

--- api_v2/models/__init__.py
+++ api_v2/models/__init__.py
@@ -6,12 +6,18 @@ from .abstracts import HasName
 from .abstracts import HasDescription
 from .abstracts import Object
 
+from .abilities import Abilities
+
 from .item import Item
 from .item import ItemSet
 
 from .armor import Armor
 
 from .weapon import Weapon
+
+from .creature import Creature
+from .creature import CreatureAction
+from .creature import CreatureAttack
 
 from .document import Document
 from .document import License
