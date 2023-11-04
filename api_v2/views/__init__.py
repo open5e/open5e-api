bf03d2c772041da0690d0f7266569e398d83298f
BuildTools <unconfigured@null.spigotmc.org>
2023-11-04 13:00:07 -0500

    Splitting Documents.

--- api_v2/views/__init__.py
+++ api_v2/views/__init__.py
@@ -6,3 +6,8 @@ from .creature import CreatureFilterSet, CreatureViewSet
 from .creature import CreatureTypeViewSet
 
 from .background import BackgroundFilterSet, BackgroundViewSet
+
+from .document import DocumentViewSet
+from .document import RulesetViewSet
+from .document import PublisherViewSet
+from .document import LicenseViewSet
--- api_v2/views/views.py
+++ api_v2/views/views.py
@@ -60,45 +60,6 @@ class ItemSetViewSet(viewsets.ReadOnlyModelViewSet):
     filterset_class = ItemSetFilterSet
 
 
-class RulesetViewSet(viewsets.ReadOnlyModelViewSet):
-    """"
-    list: API Endpoint for returning a set of rulesets.
-
-    retrieve: API endpoint for return a particular ruleset.
-    """
-    queryset = models.Ruleset.objects.all().order_by('pk')
-    serializer_class = serializers.RulesetSerializer
-
-
-class DocumentViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    list: API endpoint for returning a list of documents.
-    retrieve: API endpoint for returning a particular document.
-    """
-    queryset = models.Document.objects.all().order_by('pk')
-    serializer_class = serializers.DocumentSerializer
-    filterset_fields = '__all__'
-
-
-class PublisherViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    list: API endpoint for returning a list of publishers.
-    retrieve: API endpoint for returning a particular publisher.
-    """
-    queryset = models.Publisher.objects.all().order_by('pk')
-    serializer_class = serializers.PublisherSerializer
-    filterset_fields = '__all__'
-
-
-class LicenseViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    list: API endpoint for returning a list of licenses.
-    retrieve: API endpoint for returning a particular license.
-    """
-    queryset = models.License.objects.all().order_by('pk')
-    serializer_class = serializers.LicenseSerializer
-    filterset_fields = '__all__'
-
 
 class WeaponFilterSet(FilterSet):
 
