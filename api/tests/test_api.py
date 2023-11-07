4d8af2f6c96a4c2305d60ee635c894369c44db45
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-11-07 13:56:22 +0100

    refactor tests

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -2,20 +2,23 @@ import requests
 
 from approvaltests import verify_as_json, Options,  DiffReporter
 
+API_BASE = f"http://localhost:8000"
 
 class TestAPIRoot:
     """Test cases for testing the / root of the API."""
 
     def _verify(self, endpoint: str):
-        response = requests.get(endpoint, allow_redirects=True, headers = {'Accept': 'application/json'}).json()
+        response = requests.get(API_BASE + endpoint, allow_redirects=True, headers = {'Accept': 'application/json'}).json()
 
         verify_as_json(response, options=Options().with_reporter(DiffReporter()))
 
     def test_root(self):
-        self._verify(f"http://localhost:8000/")
+        self._verify(f"/")
 
+    # specific tests like these should be aimed at spots where there have been bugs before, to prevent regression.
     def test_magic_missile(self):
-        self._verify(f"http://localhost:8000/spells/magic-missile/")
+        self._verify(f"/spells/magic-missile/")
 
+    # enpoint tests ensure the basic structure and count of the objects does not change by accident
     def test_monsters(self):
-        self._verify(f"http://localhost:8000/monsters/")
+        self._verify(f"/monsters/")
