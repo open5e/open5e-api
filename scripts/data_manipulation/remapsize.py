2206ee2446352119d626f75bf2fa62d60aca6bf8
August Johnson <august@fastmail.com>
2024-03-14 14:33:34 -0500

    Item Size.

--- scripts/data_manipulation/remapsize.py
+++ scripts/data_manipulation/remapsize.py
@@ -5,4 +5,14 @@ from api_v2 import models as v2
 #$  python manage.py shell -c 'from scripts.data_manipulation.spell import casting_option_generate; casting_option_generate()'
 
 def remapsize():
-    print("REMAPPING SIZE")
+    print("REMAPPING SIZE FOR ITEMS")
+    for item in v2.Item.objects.all():
+        for size in v2.Size.objects.all():
+            if item.size_integer == size.rank:
+                mapped_size = size
+        print("key:{} size_int:{} mapped_size:{}".format(item.key, item.size_integer, mapped_size.name))
+        item.size = mapped_size
+        item.save()
+
+
+    print("REMAPPING SIZE FOR CREATURES")
