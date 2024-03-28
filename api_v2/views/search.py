6705d95f98467e6434bd138977d357387f2d1048
August Johnson <august@fastmail.com>
2024-03-28 14:35:14 -0500

    Stable viewset.

--- api_v2/views/search.py
+++ api_v2/views/search.py
@@ -13,24 +13,31 @@ from api_v2 import serializers
 class SearchResultViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.SearchResultSerializer
-    #https://docs.djangoproject.com/en/5.0/topics/db/sql/#mapping-query-fields-to-model-fields
-    
-    #todo:
-    # Parameterize query
-    # Pull the "text" input from the querystring
-    # See if 'v1' filters can be added "natively'
 
     def get_queryset(self):
-        #query = 'cape'
-        query = self.request.query_params.get('query')
-        schema_version = '%'
+
+        if self.request.query_params.get('query') is None:
+            # Return an empty queryset
+            return models.SearchResult.objects.none()
+        else:
+            query = self.request.query_params.get('query')
+
         if self.request.query_params.get("schema") is None:
             schema_version = '%'
-        else: 
+        else:
             schema_version = self.request.query_params.get("schema")
 
-        document_pk= '%'
-        object_route= '%'
+
+        if self.request.query_params.get("document_pk") is None:
+            document_pk= '%'
+        else:
+            document_pk = self.request.query_params.get("document_pk")
+
+        if self.request.query_params.get("object_route") is None:
+            object_route = '%'
+        else:
+            object_route = self.request.query_params.get("object_route")
+
         queryset = models.SearchResult.objects.raw(
             "SELECT 1 as id,rank,* FROM search_index " + 
             "WHERE " + 
