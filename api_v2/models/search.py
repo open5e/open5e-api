756dc61b5a8c1a18f4c76ce610da754266e39356
August Johnson <august@fastmail.com>
2024-04-02 19:33:07 -0500

    Adjusted quite a bit.

--- api_v2/models/search.py
+++ api_v2/models/search.py
@@ -5,32 +5,11 @@ class SearchResult(models.Model):
     """ The Search Result object model"""
 
     document_pk = models.CharField(max_length=255)
-    document_name = models.CharField(max_length=100)
     object_pk = models.CharField(max_length=255)
     object_name = models.CharField(max_length=100)
-    object_route = models.CharField(max_length=255)
+    object_model = models.CharField(max_length=255)
     schema_version = models.CharField(max_length=100)
 
     rank = models.DecimalField(max_digits=10, decimal_places=4, null=True, default=None)
     text = models.TextField(null=True, default=None)
     highlighted = models.TextField(null=True, default=None)
-
-    @property
-    def document_slug(self):
-        return self.document_pk
-
-    @property
-    def document_title(self):
-        return self.document_name
-
-    @property
-    def route(self):
-        return self.object_route
-
-    @property
-    def slug(self):
-        return self.object_pk
-
-    @property
-    def name(self):
-        return self.object_name
--- api_v2/serializers/search.py
+++ api_v2/serializers/search.py
@@ -10,17 +10,17 @@ class SearchResultSerializer(serializers.ModelSerializer):
     text = serializers.ReadOnlyField()
     highlighted = serializers.ReadOnlyField()
     object = serializers.SerializerMethodField(method_name='get_object')
+    document = serializers.SerializerMethodField(method_name='get_document')
 
 
     class Meta:
         model = models.SearchResult
         fields = [
-            'document_pk',
-            'document_name',
+            'document',
             'object_pk',
             'object_name',
             'object',
-            'object_route',
+            'object_model',
             'schema_version',
             'rank',
             'text',
@@ -28,18 +28,26 @@ class SearchResultSerializer(serializers.ModelSerializer):
 
     def get_object(self, obj):
         if obj.schema_version == 'v1':
-            if obj.object_route == 'magicitems/':
+            if obj.object_model == 'MagicItem':
                 result_detail = v1.MagicItem.objects.get(slug=obj.object_pk)
                 return result_detail.search_result_extra_fields()
                 
-            if obj.object_route == 'monsters/':
+            if obj.object_model == 'Monster':
                 result_detail = v1.Monster.objects.get(slug=obj.object_pk)
                 return result_detail.search_result_extra_fields()
                 
-            if obj.object_route == 'spells/':
+            if obj.object_model == 'Spell':
                 result_detail = v1.Spell.objects.get(slug=obj.object_pk)
                 return result_detail.search_result_extra_fields()
 
-            if obj.object_route == 'sections/':
+            if obj.object_model == 'Section':
                 result_detail = v1.Section.objects.get(slug=obj.object_pk)
                 return result_detail.search_result_extra_fields()
+
+    def get_document(self, obj):
+        if obj.schema_version == 'v1':
+            doc = v1.Document.objects.get(slug=obj.document_pk)
+            return {
+                'document_pk': doc.slug,
+                'document_name': doc.title
+                }
