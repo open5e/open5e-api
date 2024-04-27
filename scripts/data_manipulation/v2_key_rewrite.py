7181b1465d4d402a35467297614244dbfb480fef
August Johnson <august@fastmail.com>
2024-04-27 06:44:31 -0500

    Updating license pk.

---
+++ scripts/data_manipulation/v2_key_rewrite.py
@@ -0,0 +1,13 @@
+# Step 1:
+#Ensure rulesets are perfect.
+# Step 2: 
+#Ensure Licenses are perfect.
+#Step 3:
+# Ensure documents are perect.
+#> This will require remapping if there are renaming
+
+# Step 4:
+#Re-import.
+
+# Step 5:
+Update export to warn on keys no matching convention.
