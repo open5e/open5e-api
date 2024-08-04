a8f3e2147b2b9fbc64bcc79d2ba17e75340a62af
August Johnson <august@fastmail.com>
2024-08-03 20:33:09 -0500

    Adding a warning response header for deprecated apis.

--- server/middleware.py
+++ server/middleware.py
@@ -13,3 +13,19 @@ class NewRelicMiddleware:
         response = self.get_response(request)
 
         return response
+
+class ResponseWarningHeaderMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if request.path.startswith('/v1'):
+            response_message = "299 Deprecated API: use /v2/ instead. /v1/ will be maintained until 2024-12-31."
+
+            if request.path in ['/v1/search','/v1/search/']:
+                response_message = "299 Deprecated API: use /v2/search instead."
+ 
+            response.headers['Warning'] = response_message
+
+        return response
