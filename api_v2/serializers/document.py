8c416970d2f6741be2b1672ec3ff36869d9a9c33
Ean Moody <moody.designer@gmail.com>
2024-08-25 14:38:00 -0400

    expose ruleset & publisher name/key on documents endpoint

--- api_v2/serializers/document.py
+++ api_v2/serializers/document.py
@@ -30,7 +30,30 @@ class PublisherSerializer(serializers.HyperlinkedModelSerializer):
 
 class DocumentSerializer(GameContentSerializer):
     key = serializers.ReadOnlyField()
+    stats = serializers.ReadOnlyField()
+    publisher_name = serializers.SerializerMethodField()
+    publisher_key = serializers.SerializerMethodField()
+    ruleset_name = serializers.SerializerMethodField()
+    ruleset_key = serializers.SerializerMethodField()
 
+    def get_publisher_name(self, obj):
+        return obj.publisher.name if obj.publisher else None
+
+    def get_publisher_key(self, obj):
+        return obj.publisher.key if obj.publisher else None
+    
+    def get_publisher_name(self, obj):
+        return obj.publisher.name if obj.publisher else None
+
+    def get_publisher_key(self, obj):
+        return obj.publisher.key if obj.publisher else None
+    
+    def get_ruleset_name(self, obj):
+        return obj.ruleset.name if obj.ruleset else None
+
+    def get_ruleset_key(self, obj):
+        return obj.ruleset.key if obj.ruleset else None
+    
     class Meta:
         model = models.Document
         fields = '__all__'
