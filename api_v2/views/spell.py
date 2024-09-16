991ab7c9dd79df9d04af9bf936d69b472f2721fb
Tyler Reed <tylereed@gmail.com>
2024-09-16 14:36:03 -0600

    Bringing v2 spell filtering into line with v1

--- api_v2/views/spell.py
+++ api_v2/views/spell.py
@@ -10,11 +10,21 @@ class SpellFilterSet(FilterSet):
     class Meta:
         model = models.Spell
         fields = {
-            'key': ['in', 'iexact', 'exact' ],
-            'name': ['iexact', 'exact','contains'],
-            'document__key': ['in','iexact','exact'],
-            'document__ruleset__key': ['in','iexact','exact'],
-            'classes__key': ['in','iexact','exact']
+            'key': ['in', 'iexact', 'exact'],
+            'name': ['iexact', 'exact', 'contains', 'icontains'],
+            'document__key': ['in', 'iexact', 'exact'],
+            'document__ruleset__key': ['in', 'iexact', 'exact'],
+            'classes__key': ['in', 'iexact', 'exact'],
+            'level': ['exact', 'range', 'gt', 'gte', 'lt', 'lte'],
+            'range': ['exact', 'range', 'gt', 'gte', 'lt', 'lte'],
+            'school__name': ['in', 'iexact', 'exact'],
+            'duration': ['in', 'iexact', 'exact'],
+            'concentration': ['exact'],
+            'verbal': ['exact'],
+            'somatic': ['exact'],
+            'material': ['exact'],
+            'material_consumed': ['exact'],
+            'casting_time': ['in', 'iexact', 'exact'],
         }
 
 class SpellViewSet(viewsets.ReadOnlyModelViewSet):
