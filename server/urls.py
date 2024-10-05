f6dc571aee7e64b0fe60c316a8f8c894189e45d6
August Johnson <august@fastmail.com>
2024-10-05 07:13:18 -0500

    Getting a pass out of approvaltests.

--- server/urls.py
+++ server/urls.py
@@ -87,7 +87,7 @@ router_search.register('',views_v2.SearchResultViewSet, basename='search')
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('/', include(router.urls)),
+    path('', include(router.urls)),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     #path('search/', include('haystack.urls')),
     path('version/', views.get_version, name="version"),
