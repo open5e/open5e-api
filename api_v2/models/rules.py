054083354f8ecf29848ca5126be12811d30dd63d
calumbell <calumabell@googlemail.com>
2024-10-08 22:56:21 +0100

    added help_text to rules models

--- api_v2/models/rules.py
+++ api_v2/models/rules.py
@@ -15,15 +15,23 @@ class Rule(HasName, HasDescription, FromDocument):
   The Rule model contains information about a single rule from a larger RuleGroup.
   Each Rule is typically a paragraph or two long and might contain tables.
   """
+
   key = key_field()
-  index = models.IntegerField(default=1)
+  
+  index = models.IntegerField(
+    default=1,
+    help_text="A rule's position in the list of rules of the parent RuleSet"
+  )
+  
   ruleset = models.ForeignKey(
     RuleSet,
     on_delete=models.CASCADE,
-    related_name='rules'
+    related_name='rules',
+    help_text="The RuleSet which this Rule belongs to"
   )
   
   initialHeaderLevel = models.IntegerField(
     default=1,
-    choices=((i, i) for i in range(1,6))
+    choices=((i, i) for i in range(1,6)),
+    help_text="The header level to set rule title to"
   )
