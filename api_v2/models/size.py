02c2c43e1f947b3f28482ef9d99355b4e9ca8f93
August Johnson <august@fastmail.com>
2024-10-09 11:45:34 -0500

    Distance on size now.

--- api_v2/models/size.py
+++ api_v2/models/size.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.core.validators import MinValueValidator
 
 from .abstracts import HasName
-from .abstracts import distance_field
+from .abstracts import distance_field, distance_unit_field
 from .document import FromDocument
 
 class Size(HasName, FromDocument):
@@ -18,3 +17,10 @@ class Size(HasName, FromDocument):
         help_text='Ranking of the size, smallest has the lowest values.')
 
     space_diameter = distance_field()
+    distance_unit = distance_unit_field()
+
+    @property
+    def get_distance_unit(self):
+        if self.distance_unit is None:
+            return self.document.distance_unit
+        return self.distance_unit
--- api_v2/serializers/size.py
+++ api_v2/serializers/size.py
@@ -9,7 +9,11 @@ from .abstracts import GameContentSerializer
 class SizeSerializer(GameContentSerializer):
     """Serializer for the Size type"""
     key = serializers.ReadOnlyField()
+    distance_unit = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Size
         fields = '__all__'
+    
+    def get_distance_unit(self, Size):
+        return Size.get_distance_unit
