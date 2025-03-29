915c8611ba553d2c9fa50d3e841ea368e43fe4d7
August Johnson <august@fastmail.com>
2025-03-29 12:54:43 -0500

    Updating image to include basic info.

--- api_v2/models/image.py
+++ api_v2/models/image.py
@@ -9,9 +9,6 @@ from .enums import IMAGE_TYPES
 class Image(HasName, FromDocument):
     """This is the model related to image metadata."""
 
-    keywords = models.TextField(
-        help_text='List of keywords, to be used by the search index.')
-
     file_path = models.TextField(
         help_text='Relative path of the file, to be used in static file resolution.'
     )
@@ -24,6 +21,8 @@ class Image(HasName, FromDocument):
         help_text='What type image this is, such as icon or illustration.'
     )
 
+    def __str__(self):
+        return (self.document.name + " - " + self.name)
 
     def file_url(self):
         return "1"
--- scripts/data_manipulation/image_toolkit.py
+++ scripts/data_manipulation/image_toolkit.py
@@ -7,20 +7,55 @@ from api_v2 import models as v2_models
 import glob
 
 
-def get_images():
-    input_dir = './data/images/'
-    print("getting images in {}".format(input_dir))
+def main():
+    extract_image_objects_from_dir('./data/images/','icon','elderberry',update=False)
 
-    rel_image_paths = glob.glob(input_dir+'**/*.svg', recursive=True)
+
+def extract_image_objects_from_dir(directory, image_type, document_key, update=False):
+    # Count image files in directory
+    ifps=get_dir_images(directory=directory)
+
+    # Count image objects associated to document
+    i_objs = v2_models.Image.objects.filter(document=document_key)
+    print("Found {} Image objects in the database".format(len(i_objs)))
+
+    # For each image file in directory
+    inserted_count=0
+    for ifp in ifps:
+        name = ifp.split('/')[-1].split('.')[0].capitalize()
+        key = slugify(document_key+"_"+name)
+        document = v2_models.Document.objects.get(key=document_key)
+
+        if update:
+            if v2_models.Image.objects.filter(pk=key).exists():
+                print("Key collision when trying to insert {}".format(key))
+                continue
+
+            if v2_models.Image.objects.filter(file_path=ifp).exists():
+                print("File path collision when trying to insert {}".format(ifp))
+                continue
+            
+            v2_models.Image.objects.create(name=name,
+                                    key=key,
+                                    document=document,
+                                    file_path=ifp,
+                                    type='icon')
+            inserted_count+=1
+
+    print("Inserted {} images into the database.".format(inserted_count))
+
+
+def get_dir_images(directory):
+    dir_image_file_paths=[]
+    print("Discovering image files in {}".format(directory))
+    rel_image_paths = glob.glob(directory+'**/*.svg', recursive=True)
     rei_count=0
 
     for rep in rel_image_paths:
-        rei_count+=1
-    print("Found {} image files in {}".format(rei_count,input_dir))
-        
+        dir_image_file_paths.append(rep)
+    print("Found {} image files in {}".format(len(dir_image_file_paths),directory))
+    return dir_image_file_paths
 
-    img_count=0
-    for img in v2_models.Image.objects.all():
-        img_count+=1
-    
-    print("Found {} Image objects in the database".format(img_count))
+
+if __name__=="__main__":
+    main()
