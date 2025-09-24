1b0060ac474fe95cf4a1bae2fbd7cda2abd91c53
calumbell <calumabell@googlemail.com>
2025-09-24 22:56:43 +0100

    added core class traits tables to srd-2024 classes

--- api_v2/models/characterclass.py
+++ api_v2/models/characterclass.py
@@ -53,6 +53,7 @@ class ClassFeature(HasName, HasDescription, FromDocument):
     # Infer the type of this feature based on the `key`
     @property
     def feature_type(self) -> str:
+        if "core-traits" in self.key:       return "CORE_TRAITS_TABLE"
         if "proficiency-bonus" in self.key: return "PROFICIENCY_BONUS"
         if "proficiencies" in self.key:     return "PROFICIENCIES"
         if "equipment" in self.key:         return "STARTING_EQUIPMENT"
