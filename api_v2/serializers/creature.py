616f30b56c4adb4334a1f50cfc1be46f3b682edb
eepMoody <moody.designer@gmail.com>
2025-12-31 13:46:01 -0500

    update decimal speed fallbacks

--- api_v2/serializers/creature.py
+++ api_v2/serializers/creature.py
@@ -390,17 +390,13 @@ class CreatureSerializer(GameContentSerializer):
         fields={
             # todo: model typed as any
             "unit": serializers.StringRelatedField(),
-            # todo: model typed as any
-            "walk": serializers.StringRelatedField(),
-            # todo: model typed as any
-            "crawl": serializers.StringRelatedField(),
+            "walk": serializers.IntegerField(),
+            "crawl": serializers.IntegerField(),
             "hover": serializers.BooleanField(),
             "fly": serializers.IntegerField(allow_null=True),
             "burrow": serializers.IntegerField(allow_null=True),
-            # todo: model typed as any
-            "climb": serializers.StringRelatedField(),
-            # todo: model typed as any
-            "swim": serializers.StringRelatedField(),  
+            "climb": serializers.IntegerField(allow_null=True),
+            "swim": serializers.IntegerField(allow_null=True),
         }
     ))
     def get_speed_all(self, creature):
