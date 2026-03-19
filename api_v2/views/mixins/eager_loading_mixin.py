9843637db54602c15dc8c1bfedfaad0dd0491e06
calumbell <calumabell@googlemail.com>
2026-03-19 10:30:46 +0000

    fixed bug in EagerLoadingMixin conditional logic, closes #897

--- api_v2/views/mixins/eager_loading_mixin.py
+++ api_v2/views/mixins/eager_loading_mixin.py
@@ -14,35 +14,30 @@ class EagerLoadingMixin:
   
   ## Usage Example
   ```
-    # EagerLoadingMixin inhertired before base-case
     class CreatureViewSet(EagerLoadingMixin, viewsets.ReadOnlyModelViewSet):
       queryset = models.Creature.objects.all().order_by('pk')
       serializer_class = serializers.CreatureSerializer
       filterset_class = CreatureFilterSet
-
       select_related_fields = []   # ForeignKey relations to optimise with select_related()      
       prefetch_related_fields = [] # ManyToMany/reverse relations to optimise with prefetch_related()
   ```
   """
 
   # Override these lists in child views 
-  select_related_fields = [] # ForeignKeys to optimise
-  prefetch_related_fields = [] # ManyToMany & reverse relationships to prefetch
+  select_related_fields = []
+  prefetch_related_fields = []
 
   def get_queryset(self):
     """Override DRF's default get_queryset() method to apply eager loading"""
-    queryset = super().get_queryset()
-
-    # Get query parameters from request
-    requested_fields = self.request.query_params.get('fields', '').split(',')
-    depth = int(self.request.query_params.get('depth', 0))
-
-    # if no fields are passed via query param, select/prefetch all fields defined on the view
-    if not requested_fields:
-      queryset = queryset.select_related(*self.select_related_fields)
-      queryset = queryset.prefetch_related(*self.prefetch_related_fields)
-      return queryset
     
+    queryset = super().get_queryset()
+    requested_fields = self.request.query_params.get('fields', '').split(',')
+    
+    if not any(requested_fields):
+      return queryset \
+          .select_related(*self.select_related_fields) \
+          .prefetch_related(*self.prefetch_related_fields)
+        
     # filter selected fields against fields requested by user via query params 
     # this stops Django prefetching data that isn't even returned by this view
     select_fields = []
@@ -56,8 +51,3 @@ class EagerLoadingMixin:
     for field_to_prefetch in self.prefetch_related_fields:
       if any(field_in_request in field_to_prefetch for field_in_request in requested_fields):
         prefetch_fields.append(field_to_prefetch)
-
-    # Apply filtered optimisations
-    queryset = queryset.select_related(*select_fields)
-    queryset = queryset.prefetch_related(*prefetch_fields)
-    return queryset
