862a5a6c24f16362b0594a29d85c56057a00490a
calumbell <calumabell@googlemail.com>
2026-06-07 14:47:57 +0100

    updated nested serializers to return crossreferences

--- api_v2/serializers/species.py
+++ api_v2/serializers/species.py
@@ -8,10 +8,14 @@ from .abstracts import GameContentSerializer
 from .document import DocumentSummarySerializer
 
 class SpeciesTraitSerializer(GameContentSerializer):
-
+    crossreferences = serializers.SerializerMethodField(method_name='get_crossreferences_data')
+    def get_crossreferences_data(self, obj):
+        # delegate to parent implementation in GameContentSerializer
+        return self.get_crossreferences(obj)
+    
     class Meta:
         model = models.SpeciesTrait
-        fields = ['name', 'desc', 'type', 'order']
+        fields = ['name', 'desc', 'type', 'order', 'crossreferences']
 
 
 class SpeciesSerializer(GameContentSerializer):
